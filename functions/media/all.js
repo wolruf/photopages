694a25c30b43066d510592c3b672461079e416da
Olivier <ocahagne@fastmail.com>
2024-11-03 16:19:37 +0100

    Update all.js

--- functions/media/all.js
+++ functions/media/all.js
@@ -1,6 +1,6 @@
 export async function onRequestGet(ctx) {
   const path = new URL(ctx.request.url).pathname.replace("/media/rgc24", "rgc2024");
-  const file = await ctx.env.MEDIA.get("DSC_0001.JPG");
+  const file = await ctx.env.MEDIA.get("rgc2024/DSC_0001.JPG");
   if (!file) return new Response(null, { status: 404 });
   return new Response(file.body, {
     headers: { "Content-Type": file.httpMetadata.contentType },
